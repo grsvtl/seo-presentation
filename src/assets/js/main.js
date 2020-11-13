a5049242931fd0d5ef7fdf6bc1cb7222fc493342
Alexandr Petrovich <apalion97@gmail.com>
2020-11-13 14:32:06 +0200

    commit 13.11 v1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -16,20 +16,32 @@ import ScrollMagic from 'scrollmagic/scrollmagic/uncompressed/ScrollMagic';
 $('[data-toggle="popover"]').popover();
 $('[data-toggle="tooltip"]').tooltip();
 
-// var controller = new ScrollMagic.Controller();
-// //
-// var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 900, triggerHook: "onLeave",opacity: 0})
-//     .setPin("#scroll-topics__content")
-//     .addIndicators({name: "1 (duration: 200)"})
-//     .addTo(controller);
+var controller = new ScrollMagic.Controller();
 //
-// var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 900, triggerHook: "onLeave",})
-//     .setPin("#scroll-topics__content2")
-//     .addIndicators({name: "1 (duration: 200)"})
-//     .addTo(controller);
+var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 900, triggerHook: "onLeave",opacity: 0})
+    .setPin("#scroll-topics__content")
+    .addIndicators({name: "1 (duration: 200)"})
+    .addTo(controller);
 
+var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 900, triggerHook: "onLeave",})
+    .setPin("#scroll-topics__content2")
+    .addIndicators({name: "1 (duration: 200)"})
+    .addTo(controller);
 
+var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 900, triggerHook: "onLeave",})
+    .setPin("#scroll-topics__content3")
+    .addIndicators({name: "1 (duration: 200)"})
+    .addTo(controller);
 
+var scene4 = new ScrollMagic.Scene({triggerElement: "#trigger4", duration: 900, triggerHook: "onLeave",})
+    .setPin("#scroll-topics__content4")
+    .addIndicators({name: "1 (duration: 200)"})
+    .addTo(controller);
+
+var scene5 = new ScrollMagic.Scene({triggerElement: "#trigger5", duration: 900, triggerHook: "onLeave",})
+    .setPin("#scroll-topics__content5")
+    .addIndicators({name: "1 (duration: 200)"})
+    .addTo(controller);
 
 
 // var scene3 = new ScrollMagic.Scene({triggerElement: "#myScrollspy", triggerHook: "onLeave"})
@@ -55,3 +67,47 @@ $(document).ready(function(){
 // $(document).ready(function(){
 //     $('body').scrollspy({target: ".navbar", offset: 50});
 // });
+
+
+let isMobile = false;
+$(document).ready( function() {
+    if ($('body').width() >= 992) {
+        isMobile = true;
+    }
+    if (isMobile) {
+        (function(){
+            var a = document.querySelector('#scroll-topics__leftBlock'), b = null, P = 0;
+            window.addEventListener('scroll', Ascroll, false);
+            document.body.addEventListener('scroll', Ascroll, false);
+            function Ascroll() {
+                if (b == null) {
+                    var Sa = getComputedStyle(a, ''), s = '';
+                    for (var i = 0; i < Sa.length; i++) {
+                        if (Sa[i].indexOf('box-sizing') === 0 || Sa[i].indexOf('overflow') === 0 || Sa[i].indexOf('width') === 0 || Sa[i].indexOf('padding') === 0 || Sa[i].indexOf('border') === 0 || Sa[i].indexOf('outline') === 0 || Sa[i].indexOf('box-shadow') === 0 || Sa[i].indexOf('background') === 0) {
+                            s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+                        }
+                    }
+                    a.innerHTML = '<div style="'+s+'">'+a.innerHTML+'</div>';
+                    b = a.children[0];
+                    a.style.height = b.getBoundingClientRect().height + 'px';
+                    a.style.padding = '0';
+                    a.style.border = '0';
+                }
+                var Ra = a.getBoundingClientRect(),
+                    R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('footer').getBoundingClientRect().top + 0);  // селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
+                if ((Ra.top - P) <= 0) {
+                    if ((Ra.top - P) <= R) {
+                        b.className = 'stop';
+                        b.style.top = - R +'px';
+                    } else {
+                        b.className = 'sticky';
+                        b.style.top = P + 'px';
+                    }
+                } else {
+                    b.className = '';
+                    b.style.top = '';
+                }
+            }
+        })()
+    }
+} );
