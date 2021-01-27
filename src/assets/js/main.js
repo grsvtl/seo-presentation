7b532fa6f6a64e8be144832dd15e20a1779daff2
Alexandr Petrovich <apalion97@gmail.com>
2021-01-27 11:04:48 +0200

    commit v2 27.01

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -50,31 +50,31 @@ var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 900, 
     .setPin("#scroll-topics__content")
     .addIndicators({name: "1"})
     .setClassToggle(".scroll-topics__leftBlock-1", "active")
-    .addTo(controller);
+    //.addTo(controller);
 
 var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 900, triggerHook: 0.01})
     .setPin("#scroll-topics__content2")
     .addIndicators({name: "2"})
     .setClassToggle(".scroll-topics__leftBlock-2", "active")
-    .addTo(controller);
+    //.addTo(controller);
 
 var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 900, triggerHook: 0.1,})
     .setPin("#scroll-topics__content3")
     .addIndicators({name: "3"})
     .setClassToggle(".scroll-topics__leftBlock-3", "active")
-    .addTo(controller);
+    //.addTo(controller);
 
 var scene4 = new ScrollMagic.Scene({triggerElement: "#trigger4", duration: 900, triggerHook: 0.03,})
     .setPin("#scroll-topics__content4")
     .addIndicators({name: "4"})
     .setClassToggle(".scroll-topics__leftBlock-4", "active")
-    .addTo(controller);
+    //.addTo(controller);
 
 var scene5 = new ScrollMagic.Scene({triggerElement: "#trigger5", duration: 900, triggerHook: 0.1,})
     .setPin("#scroll-topics__content5")
     .addIndicators({name: "5"})
     .setClassToggle(".scroll-topics__leftBlock-5", "active")
-    .addTo(controller);
+    // .addTo(controller);
 
 let isMobile = false;
 $(document).ready( function() {
